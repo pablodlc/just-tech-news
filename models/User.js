869bc56277171a0f6301186db2de4bc911538349
pablo de la cruz <pablodlc@gmail.com>
2021-06-07 20:38:08 -0700

    added Post model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model { }
-
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 User.init(
     {
         id: {
@@ -53,5 +57,6 @@ User.init(
         modelName: 'user'
     }
 );
+
 
 module.exports = User;
